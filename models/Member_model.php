4904ed7580829d2b7f9ead256751ce12a02d404c
Kenta Saito <kenta.saito@sagittar.org>
2017-08-17 11:42:53 +0900

    2017-08-17 11:42:53

--- models/Member_model.php
+++ models/Member_model.php
@@ -4,31 +4,35 @@ class Member_model extends Crud_model {
 	public function __construct($params)
 	{
                 parent::__construct($params);
-		$this->has_hash = ['member_post' => 'post'];
-		$this->row_action_hash['proxy'] = 'view';
-		$this->select_hash = [
-			'member_id' => NULL,
-			'member_name' => NULL,
+		$this->overwrite('has_hash', 'member_post', 'post');
+		$this->append('row_action_hash', 'proxy', 'view');
+		$this->overwrite('select_hash', [
+			'member_id'    => NULL,
+			'member_name'  => NULL,
 			'member_email' => NULL,
-		];
-		$this->set_list = [
+		]);
+		$this->overwrite('set_list', [
 			'member_name',
 			'member_email',
 			'member_password',
-		];
-		$this->where_hash = ['simple' => 'CONCAT(`member_name`, `member_email`) LIKE "%$1%"'];
-		$this->order_by_hash = ['member_id_asc' => "`member_id` DESC"];
-		$this->use_card = TRUE;
+		]);
+		$this->overwrite('use_card', TRUE);
+		$this->append('where_hash', 'simple', 'CONCAT(`member_name`, `member_email`) LIKE "%$1%"');
+		$this->append('order_by_hash', 'member_id_desc', "`member_id` DESC");
 		switch ($this->actor)
 		{
 		case 'm':
-			$this->action_list = ['view', 'edit'];
-			$this->row_action_hash = [];
-			$this->where_list[] = "`member_id` = '{$this->auth['id']}'";
+			$this->remove('action_list', 'index');
+			$this->remove('action_list', 'add');
+			$this->remove('action_list', 'delete');
+			$this->remove('row_action_hash', 'proxy');
+			$this->append('where_list', "`member_id` = '{$this->auth['id']}'");
 			break;
 		case 'g':
-			$this->action_list = ['index', 'view'];
-			$this->row_action_hash = [];
+			$this->remove('action_list', 'add');
+			$this->remove('action_list', 'edit');
+			$this->remove('action_list', 'delete');
+			$this->remove('row_action_hash', 'proxy');
 			break;
 		}
 	}
