4423bf160df254c5710f1c37f311a775fc40e1c3
Kenta Saito <kenta.saito@sagittar.org>
2017-08-17 12:04:00 +0900

    2017-08-17 12:04:00

--- models/Admin_model.php
+++ models/Admin_model.php
@@ -4,26 +4,34 @@ class Admin_model extends Crud_model {
 	public function __construct($params)
 	{
                 parent::__construct($params);
-		$this->select_hash = [
-			'admin_id' => NULL,
-			'admin_name' => NULL,
+		$this->overwrite('select_hash', [
+			'admin_id'    => NULL,
+			'admin_name'  => NULL,
 			'admin_email' => NULL,
-		];
-		$this->set_list = [
+		]);
+		$this->overwrite('set_list', [
 			'admin_name',
 			'admin_email',
 			'admin_password',
-		];
-		$this->where_hash = ['simple' => 'CONCAT(`admin_name`, `admin_email`) LIKE "%$1%"'];
-		$this->order_by_hash = ['admin_id_asc' => "`admin_id` DESC"];
-		$this->use_card = TRUE;
+		]);
+		$this->overwrite('order_by_hash', ['admin_id_asc' => "`admin_id` DESC"]);
+		$this->overwrite('use_card',TRUE);
+		$this->append('where_hash', 'simple', 'CONCAT(`admin_name`, `admin_email`) LIKE "%$1%"');
 		switch ($this->actor)
 		{
 		case 'm':
-			$this->action_list = [];
+			$this->remove('action_list', 'index');
+			$this->remove('action_list', 'view');
+			$this->remove('action_list', 'add');
+			$this->remove('action_list', 'edit');
+			$this->remove('action_list', 'delete');
 			break;
 		case 'g':
-			$this->action_list = [];
+			$this->remove('action_list', 'index');
+			$this->remove('action_list', 'view');
+			$this->remove('action_list', 'add');
+			$this->remove('action_list', 'edit');
+			$this->remove('action_list', 'delete');
 			break;
 		}
 	}
