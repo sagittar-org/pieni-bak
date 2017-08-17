0f2f3823332f8dd89096b4b6175dfeaa835ec14c
Kenta Saito <kenta.saito@sagittar.org>
2017-08-17 12:33:51 +0900

    2017-08-17 12:33:51

--- models/File_model.php
+++ models/File_model.php
@@ -4,46 +4,43 @@ class File_model extends Crud_model {
 	public function __construct($params)
 	{
                 parent::__construct($params);
-		$this->action_list = ['index', 'add', 'edit', 'delete'];
-		$this->row_action_hash['download'] = 'view';
-		$this->select_hash = [
-			'file_id' => NULL,
-			'file_name' => NULL,
+		$this->remove('action_list', 'view');
+		$this->append('row_action_hash', 'download', 'view');
+		$this->overwrite('select_hash', [
+			'file_id'      => NULL,
+			'file_name'    => NULL,
 			'file_created' => NULL,
-			'file_file' => NULL,
-		];
-		$this->set_list = [
+			'file_file'    => NULL,
+		]);
+		$this->overwrite('set_list', [
 			'file_name',
 			'file_file',
-		];
-		$this->fixed_hash = ['file_created' => 'CURRENT_TIMESTAMP'];
-		$this->where_hash = ['simple' => '`file_name` LIKE "%$1%"'];
-		$this->order_by_hash = ['file_id_desc' => "`file_id` DESC"];
+		]);
+		$this->append('fixed_hash', 'file_created', 'CURRENT_TIMESTAMP');
+		$this->append('where_hash', 'simple', '`file_name` LIKE "%$1%"');
+		$this->overwrite('order_by_hash', ['file_id_desc' => "`file_id` DESC"]);
 		switch ($this->actor)
 		{
 		case 'm':
-			$this->action_list = [];
+			$this->remove('action_list', 'index');
+			$this->remove('action_list', 'add');
+			$this->remove('action_list', 'edit');
+			$this->remove('action_list', 'delete');
 			break;
 		case 'g':
-			$this->action_list = [];
+			$this->remove('action_list', 'index');
+			$this->remove('action_list', 'add');
+			$this->remove('action_list', 'edit');
+			$this->remove('action_list', 'delete');
 			break;
 		}
 		switch ($this->action)
 		{
 		case 'index':
-			unset($this->select_hash['file_file']);
+			$this->remove('select_hash', 'file_file');
 			break;
 		case 'delete':
-			unset($this->select_hash['file_file']);
-			break;
-		}
-		switch ($this->alias)
-		{
-		case 'file_edit':
-			unset($this->select_hash['file_created']);
-			break;
-		case 'file_delete':
-			unset($this->select_hash['file_file']);
+			$this->remove('select_hash', 'file_file');
 			break;
 		}
 	}
