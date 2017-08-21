7b0eb224e30452f95067e0643fe5c7ddd539a8fb
Kenta Saito <kenta.saito@sagittar.org>
2017-08-21 18:18:26 +0900

    2017-08-21 18:18:26

--- controllers/Controller.php
+++ controllers/Controller.php
@@ -3,6 +3,17 @@ class Controller {
 
 	public function __construct()
 	{
+		// 内部遷移ではなく、言語がデフォルト言語なら
+		if (( ! isset($_SERVER['HTTP_REFERER']) OR ! preg_match('#^'.site_url().'#', $_SERVER['HTTP_REFERER'])) && uri('language') === config('uri')['language_list'][0])
+		{
+			$lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+			// 言語が定義されていれば
+			if (in_array($lang, array_slice(config('uri')['language_list'], 1)))
+			{
+				redirect($lang.'/'.uri('uri_string'), FALSE, FALSE);
+			}
+		}
+
 		// セッション開始
 		session_name(config('session')['name']);
 		session_start();
