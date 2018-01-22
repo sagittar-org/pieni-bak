e297518d9a2717d8da4bda23eade47ce1b8542e9
Kenta Saito <kenta.saito@sagittar.org>
2018-01-22 20:54:51 +0900

    2018-01-22 20:54:51

--- views/footer.php
+++ views/footer.php
@@ -16,12 +16,12 @@ body {
     <footer class="footer">
       <nav class="navbar navbar-default" style="margin-bottom:0;">
         <div class="container">
+<?php if (in_array(uri('actor'), config('spec')['actor_list'])): ?>
             <ul class="nav navbar-nav">
               <li><a href="<?php href(''); ?>"><?php l('actor_'.uri('actor')); ?></a></li>
               <li><a href="<?php href('spec'); ?>"><?php l('spec'); ?></a></li>
             </ul>
-          <div id="navbar" class="navbar-collapse collapse">
-          </div><!--/.nav-collapse -->
+<?php endif; ?>
         </div>
       </nav>
     </footer>
