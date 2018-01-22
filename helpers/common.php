83a85e146a27cddf228e9802dd285e4f9729aa75
Kenta Saito <kenta.saito@sagittar.org>
2018-01-23 01:21:02 +0900

    2018-01-23 01:21:02

--- helpers/common.php
+++ helpers/common.php
@@ -61,7 +61,7 @@ if ( ! function_exists('exec_request'))
 		}
 
 		// composer
-		require_once __DIR__.'/../../../autoload.php';
+		require_once 'vendor/autoload.php';
 
 		// 言語ファイルを読み込み
 		$GLOBALS['language_hash'] = [];
